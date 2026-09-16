b93f5b3ec6ad2ae20a5a0f41ebf21211c9180e98
regelus19 <47474147+regelus19@users.noreply.github.com>
2026-09-16 06:28:52 -0500

    Wire manual KPI room and demand controls into War Room

--- src/views/WarRoomView.tsx
+++ src/views/WarRoomView.tsx
@@ -5,22 +5,25 @@ import { InboundDemandQueue } from '../components/InboundDemandQueue';
 import { RoutingPanel } from '../components/RoutingPanel';
 import { TeamsActionPanel } from '../components/TeamsActionPanel';
 import { RoomGrid } from '../components/RoomGrid';
-import { Room, Nurse, IncomingDemandItem, TimelineEvent, OperationalIssue, CapacityStatus } from '../types/hospital';
+import { Room, IncomingDemandItem, TimelineEvent, OperationalIssue, OperationalKpis } from '../types/hospital';
 
 interface WarRoomViewProps {
   rooms: Room[];
-  nurses: Nurse[];
   demandQueue: IncomingDemandItem[];
   timelineEvents: TimelineEvent[];
   operationalIssues: OperationalIssue[];
-  capacityStatus: CapacityStatus;
-  actionableBeds: number;
-  projectedCensus: number;
+  kpis: OperationalKpis;
   selectedPatient: IncomingDemandItem | null;
   onSelectPatient: (item: IncomingDemandItem) => void;
   onCloseRouting: () => void;
   onUpdateRoute: (patientId: string, newPreferred: string) => void;
   onAddIssue: (newIssue: OperationalIssue) => void;
+  onUpdateKpis: (next: OperationalKpis) => void;
+  onResetKpis: () => void;
+  onUpdateRoom: (room: Room) => void;
+  onAddDemand: (item: IncomingDemandItem) => void;
+  onUpdateDemand: (item: IncomingDemandItem) => void;
+  onDeleteDemand: (id: string) => void;
 }
 
 export const WarRoomView: React.FC<WarRoomViewProps> = ({
@@ -28,22 +31,37 @@ export const WarRoomView: React.FC<WarRoomViewProps> = ({
   demandQueue,
   timelineEvents,
   operationalIssues,
-  capacityStatus,
-  actionableBeds,
-  projectedCensus,
+  kpis,
   selectedPatient,
   onSelectPatient,
   onCloseRouting,
   onUpdateRoute,
-  onAddIssue
+  onAddIssue,
+  onUpdateKpis,
+  onResetKpis,
+  onUpdateRoom,
+  onAddDemand,
+  onUpdateDemand,
+  onDeleteDemand
 }) => (
   <div className="space-y-4">
-    <KPIRow census={42} physicalBeds={46} staffedBeds={44} actionableBeds={actionableBeds} expectedDischarges={14} expectedArrivals={14} edHolds={2} pendingTransfers={3} capacityStatus={capacityStatus} projectedCensus={projectedCensus} />
+    <KPIRow kpis={kpis} onUpdate={onUpdateKpis} onReset={onResetKpis} />
     <DemandTimeline events={timelineEvents} />
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-      <div className="lg:col-span-2"><InboundDemandQueue demandQueue={demandQueue} onSelectPatient={onSelectPatient} /></div>
-      <div className="space-y-4"><RoutingPanel selectedPatient={selectedPatient} onClose={onCloseRouting} onUpdateRoute={onUpdateRoute} /><TeamsActionPanel issues={operationalIssues} onAddIssue={onAddIssue} /></div>
+      <div className="lg:col-span-2">
+        <InboundDemandQueue
+          demandQueue={demandQueue}
+          onSelectPatient={onSelectPatient}
+          onAddDemand={onAddDemand}
+          onUpdateDemand={onUpdateDemand}
+          onDeleteDemand={onDeleteDemand}
+        />
+      </div>
+      <div className="space-y-4">
+        <RoutingPanel selectedPatient={selectedPatient} onClose={onCloseRouting} onUpdateRoute={onUpdateRoute} />
+        <TeamsActionPanel issues={operationalIssues} onAddIssue={onAddIssue} />
+      </div>
     </div>
-    <RoomGrid rooms={rooms} />
+    <RoomGrid rooms={rooms} onUpdateRoom={onUpdateRoom} />
   </div>
 );
